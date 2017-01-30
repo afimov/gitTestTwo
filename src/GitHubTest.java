73ac2e91fb850af91c906657cf1e0c5c7430a4a3
Anton <anton@MacBook-Pro-Anton.local>
2017-01-30 16:30:38 +0300

    first commit

---
+++ src/GitHubTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by anton on 30.01.17.
+ */
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println("OOOH, I did some changes on my local computer");
+    }
+}
